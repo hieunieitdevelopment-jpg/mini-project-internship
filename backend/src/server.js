9f36b23a4dd19fa908d84e2561e0988546977924
Hieu <hieunieitdevelopment@gmail.com>
2026-03-12 04:56:03 +0000

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Admin Map Backend Running 🚀')
+  res.send('Admin Map Backend Running')
 })
 
 async function startServer() {
-  await connectDB()   // ⬅️ connect DB trước
+  await connectDB()   
 
   app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
   })
 }
 
