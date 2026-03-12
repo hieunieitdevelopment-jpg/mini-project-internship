a74addda7fca7a38c05b6ebd0593c2f8d6519255
Hieu <hieunieitdevelopment@gmail.com>
2026-03-12 03:56:48 +0000

    update backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const { connectDB } = require('./db')
+const { connectDB } = require('./config/db')
 
 const app = express()
 const PORT = process.env.PORT || 3000
