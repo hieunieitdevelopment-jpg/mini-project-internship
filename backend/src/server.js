94bc1170de6b6e1c96b4ad016b07a7f6a627b5e6
Hieu <hieunieitdevelopment@gmail.com>
2026-03-12 09:18:19 +0000

    design database schema for administrative address

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const { connectDB } = require('./config/db')
+const routes = require("./routes");
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+
+
+app.use("/api/v1", routes);
 
 app.get('/', (req, res) => {
   res.send('Admin Map Backend Running')
