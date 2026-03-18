89fee1a05a970004dd8d1719c97b7381c7771b4d
Thanh Phong <ntp14042004@gmail.com>
2026-03-18 12:10:29 +0700

    feat: add upload route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,10 +5,12 @@ const provinceRoutes = require("./provinceRoutes");
 const districtRoutes = require("./districtRoutes");
 const unitRoutes = require("./unitRoutes");
 const mappingRoutes = require("./mappingRoutes");
+const uploadRoutes = require("./uploadRoutes")
 
 router.use("/provinces", provinceRoutes);
 router.use("/districts", districtRoutes);
 router.use("/units", unitRoutes);
 router.use("/mappings", mappingRoutes);
+router.use("/api/v1", uploadRoutes)
 
 module.exports = router;
