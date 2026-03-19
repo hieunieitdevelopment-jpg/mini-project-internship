03632801ecf4f5e9456c548454dab354d30d628f
Le Van Khai <khaiitdeverloper@gmail.com>
2026-03-19 10:04:42 +0700

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AppRouter from "./router/AppRouter";
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Header />
       <AppRouter />
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 export default App; 
